72531b988a9f3d4c84112c615c8abb6a2a2d2e10
bobsabt <bodai.barbara@gmail.com>
2022-02-20 15:56:32 +0100

    function numbertostring -add the base expressions and zero handle

--- src/App.js
+++ src/App.js
@@ -6,15 +6,42 @@ function App() {
   const [numberIsValid, setNumberIsValid] = React.useState(true);
   const [result, setResult] = React.useState();
 
-  const onClickConvert = () => {
-    console.log("hello")
+  const onClickConvert = (string) => {
+    if(!string.includes(".")){
+      //console.log("true");
+      setNumberIsValid(true);
+      numberToString(string)
+
+
+    }
+    else{
+      //console.log("false");
+      setNumberIsValid(false);
+    }
+    
+  }
+
+  const numberToString = (string) => {
+
+    if( parseInt( string ) === 0 ) {
+      setResult("zero");
+    }
+
+    //Array of numbers until 20
+    const numberUntil20 = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+
+    //Array of tens numbers
+    const tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" ];
+
+    //Array of numbers bigger then tens
+    const hundreds = ["thousand", "million", "billion", "trillion"];
   }
   
   return (
     <div className="App">
-      <input type="text" placeholder='Write your number here...' value={numberAsString} onChange={e=>setNumberAsString(e.target.value)}/>
-      <button onClick={onClickConvert}>Convert</button>
-      {!numberIsValid && <p>Please use just number digit</p>}
+      <input type="number" placeholder='Write your number here...' value={numberAsString} onChange={e=>setNumberAsString(e.target.value)}/>
+      <button onClick={()=>onClickConvert(numberAsString)}>Convert</button>
+      {!numberIsValid && <p>Please write a whole number</p>}
       <p>{result}</p>
        
     </div>
